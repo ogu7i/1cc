3a4d12835cd20ad8278df142bc3eea5c911bbd99
ogu7i <26869932+ogu7i@users.noreply.github.com>
2023-10-24 17:45:21 +0900

    ポインタの加減算を扱えるように

--- test.sh
+++ test.sh
@@ -84,14 +84,17 @@ assert 55 '{ i=0; j=0; for (i=0; i<=10; i=i+1) j=i+j; return j; }'
 assert 3 '{ for (;;) { return 3; } return 5; }'
 assert 3 '{ i=0; for (;;) { return 3; } return 5; }'
 
-# 単項&と単項*
+# ポインタ
 assert 3 '{ x=3; return *&x; }'
 assert 3 '{ x=3; y=&x; z=&y; return **z; }'
-assert 5 '{ x=3; y=5; return *(&x+8); }'
-assert 3 '{ x=3; y=5; return *(&y-8); }'
+assert 5 '{ x=3; y=5; return *(&x+1); }'
+assert 3 '{ x=3; y=5; return *(&y-1); }'
+assert 5 '{ x=3; y=5; return *(&x-(-1)); }'
 assert 5 '{ x=3; y=&x; *y=5; return x; }'
-assert 7 '{ x=3; y=5; *(&x+8)=7; return y; }'
-assert 7 '{ x=3; y=5; *(&y-8)=7; return x; }'
+assert 7 '{ x=3; y=5; *(&x+1)=7; return y; }'
+assert 7 '{ x=3; y=5; *(&y-2+1)=7; return x; }'
+assert 5 '{ x=3; return (&x+2)-&x+3; }'
+assert 2 '{ x=1; y=3; z=5; return &z - &x; }'
 
 echo OK
 
