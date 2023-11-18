6771aaf2f06a684b50f9f6e000506b879cfb9719
ogu7i <26869932+ogu7i@users.noreply.github.com>
2023-11-18 14:47:31 +0900

    char、short、intの計算時に32ビットレジスタを使うように

--- codegen.c
+++ codegen.c
@@ -176,38 +176,47 @@ static void gen_expr(Node *node) {
   gen_expr(node->lhs);
   pop("rdi");
 
+  char *ax, *di;
+  if (node->lhs->ty->kind == TY_LONG || node->lhs->ty->base) {
+    ax = "rax";
+    di = "rdi";
+  } else {
+    ax = "eax";
+    di = "edi";
+  }
+
   switch (node->kind) {
     case ND_ADD:
-      println("  add rax, rdi");
+      println("  add %s, %s", ax, di);
       return;
     case ND_SUB:
-      println("  sub rax, rdi");
+      println("  sub %s, %s", ax, di);
       return;
     case ND_MUL:
-      println("  imul rax, rdi");
+      println("  imul %s, %s", ax, di);
       return;
     case ND_DIV:
-      println("  cqo");
-      println("  idiv rdi");
+      if (node->lhs->ty->size == 8)
+        println("  cqo");
+      else
+        println("  cdq");
+      println("  idiv %s", di);
       return;
     case ND_EQ:
-      println("  cmp rax, rdi");
-      println("  sete al");
-      println("  movzb rax, al");
-      return;
     case ND_NE:
-      println("  cmp rax, rdi");
-      println("  setne al");
-      println("  movzb rax, al");
-      return;
     case ND_LT:
-      println("  cmp rax, rdi");
-      println("  setl al");
-      println("  movzb rax, al");
-      return;
     case ND_LE:
-      println("  cmp rax, rdi");
-      println("  setle al");
+      println("  cmp %s, %s", ax, di);
+      
+      if (node->kind == ND_EQ)
+        println("  sete al");
+      else if(node->kind == ND_NE)
+        println("  setne al");
+      else if(node->kind == ND_LT)
+        println("  setl al");
+      else if(node->kind == ND_LE)
+        println("  setle al");
+
       println("  movzb rax, al");
       return;
   }
