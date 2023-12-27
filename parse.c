db0892a9ff2298deb9df80b81e8596e6d870f4ff
ogu7i <26869932+ogu7i@users.noreply.github.com>
2023-12-27 10:17:27 +0900

    末尾に余分なコンマがあっても良いように

--- parse.c
+++ parse.c
@@ -764,10 +764,28 @@ static Type *declspec(Token **rest, Token *tok, VarAttr *attr) {
   return ty;
 }
 
+static bool is_end(Token *tok) {
+  return equal(tok, "}") || (equal(tok, ",") && equal(tok->next, "}"));
+}
+
+static bool consume_end(Token **rest, Token *tok) {
+  if (equal(tok, "}")) {
+    *rest = tok->next;
+    return true;
+  }
+
+  if (equal(tok, ",") && equal(tok->next, "}")) {
+    *rest = tok->next->next;
+    return true;
+  }
+
+  return false;
+}
+
 // enum-specifier = ident? "{" enum-list? "}"
 //                | ident ("{" enum-list? "}")?
 //
-// enum-list      = ident ("=" const_expr)? ("," ident ("=" const_expr)?)*
+// enum-list      = ident ("=" const_expr)? ("," ident ("=" const_expr)?)* ","?
 static Type *enum_specifier(Token **rest, Token *tok) {
   Type *ty = enum_type();
 
@@ -793,7 +811,7 @@ static Type *enum_specifier(Token **rest, Token *tok) {
 
   int i = 0;
   int val = 0;
-  while (!equal(tok, "}")) {
+  while (!consume_end(rest, tok)) {
     if (i++ > 0)
       tok = skip(tok, ",");
 
@@ -807,8 +825,6 @@ static Type *enum_specifier(Token **rest, Token *tok) {
     sc->enum_ty = ty;
     sc->enum_val = val++;
   }
-
-  *rest = tok->next;
 
   if (tag)
     push_tag_scope(tag, ty);
@@ -977,7 +993,7 @@ static int count_array_init_elements(Token *tok, Type *ty) {
   Initializer *dummy = new_initializer(ty->base, false);
   int i = 0;
 
-  for (; !equal(tok, "}"); i++) {
+  for (; !consume_end(&tok, tok); i++) {
     if (i > 0)
       tok = skip(tok, ",");
     initializer2(&tok, tok, dummy);
@@ -985,7 +1001,7 @@ static int count_array_init_elements(Token *tok, Type *ty) {
   return i;
 }
 
-// array-initializer1 = "{" initializer ("," initializer)* "}"
+// array-initializer1 = "{" initializer ("," initializer)* ","? "}"
 static void array_initializer1(Token **rest, Token *tok, Initializer *init) {
   tok = skip(tok, "{");
 
@@ -994,7 +1010,7 @@ static void array_initializer1(Token **rest, Token *tok, Initializer *init) {
     *init = *new_initializer(array_of(init->ty->base, len), false);
   }
   
-  for (int i = 0; !consume(rest, tok, "}"); i++) {
+  for (int i = 0; !consume_end(rest, tok); i++) {
     if (i > 0)
       tok = skip(tok, ",");
 
@@ -1011,7 +1027,7 @@ static void array_initializer2(Token **rest, Token *tok, Initializer *init) {
     *init = *new_initializer(array_of(init->ty->base, len), false);
   }
 
-  for (int i = 0; i < init->ty->array_len && !equal(tok, "}"); i++) {
+  for (int i = 0; i < init->ty->array_len && !is_end(tok); i++) {
     if (i > 0)
       tok = skip(tok, ",");
     initializer2(&tok, tok, init->children[i]);
@@ -1026,7 +1042,7 @@ static void struct_initializer1(Token **rest, Token *tok, Initializer *init) {
 
   Member *mem = init->ty->members;
 
-  while (!consume(rest, tok, "}")) {
+  while (!consume_end(rest, tok)) {
     if (mem != init->ty->members)
       tok = skip(tok, ",");
 
@@ -1042,7 +1058,7 @@ static void struct_initializer1(Token **rest, Token *tok, Initializer *init) {
 static void struct_initializer2(Token **rest, Token *tok, Initializer *init) {
   bool first = true;
 
-  for (Member *mem = init->ty->members; mem && !equal(tok, "}"); mem = mem->next) {
+  for (Member *mem = init->ty->members; mem && !is_end(tok); mem = mem->next) {
     if (!first)
       tok = skip(tok, ",");
     first = false;
@@ -1058,6 +1074,7 @@ static void union_initializer(Token **rest, Token *tok, Initializer *init) {
   // 共用体の最初のメンバを初期化する。
   if (equal(tok, "{")) {
     initializer2(&tok, tok->next, init->children[0]);
+    consume(&tok, tok, ",");
     *rest = skip(tok, "}");
   } else {
     initializer2(rest, tok, init->children[0]);
